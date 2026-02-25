3d4bdef868f32dfc4551c788a5b097aef5bb8a3f
drigusmao <adriana.gusmao@outlook.com>
2026-02-25 22:58:30 +0000

    esboço

---
+++ resource/01__origem/New File 2026-02-25 19_50_36.py
@@ -0,0 +1,7 @@
+- jogar arquivos do kaglle direto para ca
+
+-VER NECESSIDADE DESSAS PASTAS TENDO EM VISTA NAO ESTAR USANDO O AZURE IGUAL A VIDEO AULA
+
+- armazenar em variaveis o caminho de todas as pastas isso se chama mapeando as pastas
+
+- display para vixualizar 
